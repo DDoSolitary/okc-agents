6dc1dd26b04b7e1100ed75311d8b998c661ee9bf
DDoSolitary <DDoSolitary@gmail.com>
2019-09-12 15:06:32 +0800

    Add build-termux.sh.

---
+++ build-termux.sh
@@ -0,0 +1,9 @@
+if [[ -z $ANDROID_SDK_ROOT ]]; then
+	echo Android SDK not found!
+	exit
+fi
+
+export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_SDK_ROOT/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang
+export RUSTFLAGS="-C link-arg=-Wl,-rpath=/data/data/com.termux/files/usr/lib -C link-arg=-Wl,--enable-new-dtags"
+
+cargo build --target=aarch64-linux-android $@
