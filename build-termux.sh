ff9d7636de1e0acb30386bd9f541ecdae72780c2
DDoSolitary <DDoSolitary@gmail.com>
2020-07-23 23:26:54 +0800

    Build for all architectures.

--- build-termux.sh
+++ build-termux.sh
@@ -5,7 +5,12 @@ if [[ -z $ANDROID_SDK_ROOT ]]; then
 	exit
 fi
 
-export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=$ANDROID_SDK_ROOT/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang
+TARGET=${TARGET:-aarch64-linux-android}
+TARGET_ENV=${TARGET^^}
+TARGET_ENV=${TARGET_ENV//-/_}
+NDK_TARGET=${TARGET/armv7-/armv7a-}
+
+export CARGO_TARGET_${TARGET_ENV}_LINKER=$ANDROID_SDK_ROOT/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/${NDK_TARGET}30-clang
 export RUSTFLAGS="-C link-arg=-Wl,-rpath=/data/data/com.termux/files/usr/lib -C link-arg=-Wl,--enable-new-dtags"
 
-cargo build --target=aarch64-linux-android $@
+cargo build --target=$TARGET $@
