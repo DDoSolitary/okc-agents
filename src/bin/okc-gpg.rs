58b1f542c8c4398042f0b3213e51601405c72a9d
DDoSolitary <DDoSolitary@gmail.com>
2020-07-25 16:00:54 +0800

    Add logging messages.

--- src/bin/okc-gpg.rs
+++ src/bin/okc-gpg.rs
@@ -1,10 +1,21 @@
 extern crate base64;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate slog;
+extern crate slog_async;
+extern crate slog_envlogger;
+extern crate slog_term;
 extern crate tokio;
 extern crate okc_agents;
 
 use std::error::Error;
 use std::net::SocketAddr;
 use std::process::{Command, Stdio};
+use std::sync::Mutex;
+use slog::{Logger, Drain};
+use slog_async::{Async, AsyncGuard};
+use slog_term::{FullFormat, TermDecorator};
 use tokio::prelude::*;
 use tokio::fs::File;
 use tokio::io;
@@ -12,8 +23,15 @@ use tokio::net::{TcpListener, TcpStream};
 use tokio::stream::StreamExt;
 use okc_agents::utils::*;
 
-fn exit_error(e: Box<dyn Error>) -> ! {
-	eprintln!("Error: {:?}", e);
+lazy_static! {
+	static ref LOG_GUARD: Mutex<Option<AsyncGuard>> = Mutex::new(None);
+}
+
+fn exit_error(e: Box<dyn Error>, logger: Logger) -> ! {
+	error!(logger, "{:?}", e);
+	if let Some(guard) = LOG_GUARD.lock().unwrap().take() {
+		std::mem::drop(guard);
+	}
 	std::process::exit(1)
 }
 
@@ -26,66 +44,81 @@ async fn read_str<T: AsyncRead + Unpin>(rx: &mut T) -> std::result::Result<Strin
 	Ok(String::from_utf8(str_buf)?)
 }
 
-async fn handle_control_connection(mut stream: TcpStream) -> Result {
+async fn handle_control_connection(mut stream: TcpStream, logger: Logger) -> Result {
+	info!(logger, "control connection established");
 	loop {
 		let msg = read_str(&mut stream).await?;
+		debug!(logger, "new warning message received"; "length" => msg.len());
 		match msg.is_empty() {
 			true => break,
-			false => eprintln!("{}", msg)
+			false => warn!(logger, "{}", msg)
 		}
 	}
+	debug!(logger, "all warnings processed, waiting for status code");
 	let mut stat_buf = [0u8; 1];
 	stream.read_exact(&mut stat_buf).await?;
+	info!(logger, "control connection finished"; "status_code" => stat_buf[0]);
 	match stat_buf[0] {
 		0 => Ok(()),
-		_ => Err(Box::new(StringError(String::from("An error has occurred."))) as Box<dyn Error>)
+		_ => Err(Box::new(StringError(String::from("an error has occurred in the app"))) as Box<dyn Error>)
 	}
 }
 
-async fn handle_input_connection(mut stream: TcpStream) -> Result {
+async fn handle_input_connection(mut stream: TcpStream, logger: Logger) -> Result {
 	let path = read_str(&mut stream).await?;
-	if path == "-" {
+	info!(logger, "input connection established"; "path" => &path);
+	if &path == "-" {
 		let mut stdin = io::stdin();
+		debug!(logger, "reading from stdin");
 		io::copy(&mut stdin, &mut stream).await?;
 	} else {
 		let mut file = File::open(&path).await?;
+		debug!(logger, "reading from file");
 		io::copy(&mut file, &mut stream).await?;
 	}
+	info!(logger, "input connection finished");
 	Ok(())
 }
 
-async fn handle_output_connection(mut stream: TcpStream) -> Result {
+async fn handle_output_connection(mut stream: TcpStream, logger: Logger) -> Result {
 	let path = read_str(&mut stream).await?;
-	if path == "-" {
+	info!(logger, "output connection established"; "path" => &path);
+	if &path == "-" {
 		let mut stdout = io::stdout();
+		debug!(logger, "writing to stdout");
 		io::copy(&mut stream, &mut stdout).await?;
 	} else {
 		let mut file = File::create(&path).await?;
+		debug!(logger, "writing to file");
 		io::copy(&mut stream, &mut file).await?;
 	}
+	info!(logger, "output connection finished");
 	Ok(())
 }
 
-async fn handle_connection(accept_result: std::result::Result<TcpStream, tokio::io::Error>) -> Result {
+async fn handle_connection(accept_result: std::result::Result<TcpStream, tokio::io::Error>, logger: Logger) -> Result {
 	let mut stream = accept_result?;
+	let logger = logger.new(o!("remote_port" => stream.peer_addr()?.port()));
+	debug!(logger, "connection accepted");
 	let mut op = [0u8];
 	stream.read_exact(&mut op).await?;
+	debug!(logger, "connection type is {}", op[0]);
 	match op[0] {
-		0 => match handle_control_connection(stream).await {
+		0 => match handle_control_connection(stream, logger).await {
 			Ok(_) => std::process::exit(0),
 			Err(e) => Err(e)
 		},
-		1 => handle_input_connection(stream).await,
-		2 => handle_output_connection(stream).await,
-		_ => Err(Box::new(StringError(String::from("Protocol error."))) as Box<dyn Error>)
+		1 => handle_input_connection(stream, logger).await,
+		2 => handle_output_connection(stream, logger).await,
+		_ => Err(Box::new(StringError(String::from("protocol error: invalid connection type"))) as Box<dyn Error>)
 	}
 }
 
-#[tokio::main]
-async fn main() -> Result {
+async fn run(logger: Logger) -> Result {
 	let addr = "127.0.0.1:0".parse::<SocketAddr>()?;
 	let mut listener = TcpListener::bind(&addr).await?;
 	let addr = listener.local_addr()?;
+	info!(logger, "listening on port {}", addr.port());
 	let mut cmd = Command::new("am");
 	cmd.arg("broadcast")
 		.arg("-n").arg("org.ddosolitary.okcagent/.GpgProxyReceiver")
@@ -94,13 +127,29 @@ async fn main() -> Result {
 	if std::env::args().len() > 1 {
 		cmd.arg("--esa").arg("org.ddosolitary.okcagent.extra.GPG_ARGS")
 			.arg(std::env::args().skip(1).map(|s| base64::encode(&s)).collect::<Vec<_>>().join(","));
+	} else {
+		debug!(logger, "no arguments specified, GPG_ARGS won't be sent")
 	}
 	cmd.status()?;
+	info!(logger, "broadcast sent, waiting for app to connect");
 	let mut incoming = listener.incoming();
 	while let Some(accept_result) = incoming.next().await {
-		if let Err(e) = handle_connection(accept_result).await {
-			exit_error(e)
+		debug!(logger, "new incoming connection");
+		if let Err(e) = handle_connection(accept_result, logger.clone()).await {
+			exit_error(e, logger)
 		}
 	};
 	Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+	let drain = FullFormat::new(TermDecorator::new().stderr().build()).build().ignore_res();
+	let drain = slog_envlogger::new(drain).ignore_res();
+	let (drain, guard) = Async::new(drain).build_with_guard();
+	*LOG_GUARD.lock().unwrap() = Some(guard);
+	let logger = Logger::root(drain.ignore_res(), o!());
+	if let Err(e) = run(logger.clone()).await {
+		exit_error(e, logger);
+	}
 }
--- src/bin/okc-ssh-agent.rs
+++ src/bin/okc-ssh-agent.rs
@@ -1,10 +1,22 @@
 extern crate ctrlc;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate slog;
+extern crate slog_async;
+extern crate slog_envlogger;
+extern crate slog_term;
 extern crate tokio;
 extern crate okc_agents;
 
 use std::net::SocketAddr;
 use std::process::{Command, Stdio};
+use std::sync::Mutex;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::Duration;
+use slog::{Logger, Drain};
+use slog_async::{Async, AsyncGuard};
+use slog_term::{FullFormat, TermDecorator};
 use futures_util::future;
 use tokio::prelude::*;
 use tokio::net::TcpListener;
@@ -18,59 +30,92 @@ type ClientStream = tokio::net::UnixStream;
 #[cfg(not(unix))]
 type ClientStream = tokio::net::TcpStream;
 
+lazy_static! {
+	static ref LOG_GUARD: Mutex<Option<AsyncGuard>> = Mutex::new(None);
+	static ref CONN_COUNTER: AtomicU64 = AtomicU64::new(0);
+}
+
+fn exit_process(code: i32) -> ! {
+	if let Some(guard) = LOG_GUARD.lock().unwrap().take() {
+		std::mem::drop(guard);
+	}
+	std::process::exit(code)
+}
+
 async fn do_copy<T1: AsyncRead + Unpin, T2: AsyncWrite + Unpin>(rx: &mut T1, tx: &mut T2) -> Result {
 	io::copy(rx, tx).await?;
 	tx.shutdown().await?;
 	Ok(())
 }
 
-async fn handle_connection(accept_result: std::result::Result<ClientStream, io::Error>) -> Result {
+async fn handle_connection(accept_result: std::result::Result<ClientStream, io::Error>, logger: Logger) -> Result {
 	let mut client_stream = accept_result?;
+	let logger = logger.new(o!("id" => CONN_COUNTER.fetch_add(1, Ordering::Relaxed)));
+	info!(logger, "connected to client");
 	let (mut crx, mut ctx) = client_stream.split();
 	let addr = "127.0.0.1:0".parse::<SocketAddr>()?;
 	let mut app_listener = TcpListener::bind(&addr).await?;
 	let addr = app_listener.local_addr()?;
+	info!(logger, "listening on port {}", addr.port());
 	Command::new("am").arg("broadcast")
 		.arg("-n").arg("org.ddosolitary.okcagent/.SshProxyReceiver")
 		.arg("--ei").arg("org.ddosolitary.okcagent.extra.PROXY_PORT").arg(addr.port().to_string())
 		.stdout(Stdio::null()).stderr(Stdio::null())
 		.status()?;
+	info!(logger, "broadcast sent, waiting for app to connect");
 	let mut app_stream = time::timeout(Duration::from_secs(10), app_listener.incoming().next()).await
-		.map_err(|_| StringError(String::from("Timed out waiting for app to connect.")))?.unwrap()?;
+		.map_err(|_| StringError(String::from("timed out waiting for app to connect")))?.unwrap()?;
+	info!(logger, "app connected, start forwarding"; "remote_port" => app_stream.peer_addr()?.port());
 	let (mut arx, mut atx) = app_stream.split();
 	let (r1, r2) = future::join(do_copy(&mut crx, &mut atx), do_copy(&mut arx, &mut ctx)).await;
 	r1?;
 	r2?;
+	info!(logger, "connection finished");
 	Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result {
+async fn run(logger: Logger) -> Result {
 	let args = std::env::args().collect::<Vec<_>>();
 	let path = args.get(1)
-		.ok_or(StringError(String::from("Please specify path of the agent socket.")))?;
+		.ok_or(StringError(String::from("please specify path of the agent socket")))?;
 
 	#[cfg(unix)] let mut listener = tokio::net::UnixListener::bind(&path)?;
 	#[cfg(not(unix))] let mut listener = TcpListener::bind(path.parse::<SocketAddr>()?).await?;
+	info!(logger, "listening on the Unix socket: \"{}\"", path);
 
 	#[cfg(unix)] {
 		let path = path.clone();
+		let logger = logger.clone();
 		ctrlc::set_handler(move || {
 			if let Err(e) = std::fs::remove_file(&path) {
-				eprintln!("Error: {:?}", e);
-				std::process::exit(1);
+				error!(logger, "failed to delete the socket file: {:?}", e);
+				exit_process(1);
 			}
-			std::process::exit(0);
+			exit_process(0);
 		}).unwrap();
 	}
 
 	let mut incoming = listener.incoming();
 	while let Some(accept_result) = incoming.next().await {
-		if let Err(e) = handle_connection(accept_result).await {
-			eprintln!("Error: {:?}", e);
+		debug!(logger, "new incoming connection");
+		if let Err(e) = handle_connection(accept_result, logger.clone()).await {
+			error!(logger, "failed to accept the connection: {:?}", e);
 		}
 	}
 	#[cfg(unix)] std::fs::remove_file(&path)?;
 
 	Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+	let drain = FullFormat::new(TermDecorator::new().stderr().build()).build().ignore_res();
+	let drain = slog_envlogger::new(drain).ignore_res();
+	let (drain, guard) = Async::new(drain).build_with_guard();
+	*LOG_GUARD.lock().unwrap() = Some(guard);
+	let logger = Logger::root(drain.ignore_res(), o!());
+	if let Err(e) = run(logger.clone()).await {
+		error!(logger, "{:?}", e);
+		exit_process(1);
+	}
 }
